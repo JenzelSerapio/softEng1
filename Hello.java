38f00301f910b33082ffbb3948b7f1cc443b9000
JenzelSerapio <jenzelserapio08@gmail.com>
2023-08-23 16:20:37 +0800

    added a new file MyNewFile.txt

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is SERAPIO, Jenzel Leah M.");
+System.out.println("That is why developers are highly-paid!");
+
   }
 }
   
